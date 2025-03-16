de66da143397916d27bc3907c8f14361f71a2a13
namelessvoid <simon_kerler@web.de>
2025-03-16 20:34:13 +0100

    Refactor domain into ChartAxisDomain class

--- addons/easy_charts/control_charts/plotters/function_plotter.gd
+++ addons/easy_charts/control_charts/plotters/function_plotter.gd
@@ -2,8 +2,8 @@ extends Control
 class_name FunctionPlotter
 
 var function: Function
-var x_domain: Dictionary
-var y_domain: Dictionary
+var x_domain: ChartAxisDomain
+var y_domain: ChartAxisDomain
 
 func _init(function: Function) -> void:
     self.function = function
@@ -11,13 +11,13 @@ func _init(function: Function) -> void:
 func _ready() -> void:
     set_process_input(get_chart_properties().interactive)
 
-func update_values(x_domain: Dictionary, y_domain: Dictionary) -> void:
-    self.visible = self.function.get_visibility()
-    if not self.function.get_visibility():
-        return
-    self.x_domain = x_domain
-    self.y_domain = y_domain
-    queue_redraw()
+func update_values(x_domain: ChartAxisDomain, y_domain: ChartAxisDomain) -> void:
+	self.visible = self.function.get_visibility()
+	if not self.function.get_visibility():
+		return
+	self.x_domain = x_domain
+	self.y_domain = y_domain
+	queue_redraw()
 
 func _draw() -> void:
     return
